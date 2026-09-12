6a4068d36f96e215c7719c8e2a08a659deaa17dc
MD Shayaan Arif <mohammadshayaan2030@gmail.com>
2026-09-12 19:04:45 +0530

    Time: 60 ms (1.26%), Space: 7.9 MB (8.86%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
+    bool isprime(int n){
+        if(n<2) return false;
+        
+        for(int i=2;i<=n/i;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
     bool isUgly(int n) {
-        if(n==0) return false;
-        while(n%2==0){
-            n=n/2;
+        if(n==2 || n==3 || n==5) return true;
+        if(isprime(n)) return false;
+        if(n<=0) return false;
+        
+        vector<int> v;
+        for(int i=1;(long long)i*i<=n;i++){
+                if(n%i == 0 && isprime(i)){
+                    v.push_back(i);
+                }
+                if(n%i==0 && n/i !=i && isprime(n/i)){
+                    v.push_back(n/i);
+                }
+        }        
+        
+        for(int i=0;i<v.size();i++){
+                if(v[i]!=2 && v[i] !=3 && v[i]!=5){
+                    return false;
+                }
         }
-        while(n%3==0){
-            n=n/3;
-        }
-        while(n%5==0){
-            n=n/5;
-        }
-        if(n==1){
-            return true;
-        }
-        return false;
+        return true;
         
     }
 };
