748b1f4e2a52a94d64ad539bf5e1a711fa85d3b6
MD Shayaan Arif <mohammadshayaan2030@gmail.com>
2026-09-13 13:49:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.7%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while(num%10!=num){
-            int sum=0;
-            while(num>0){
-                int temp = num%10;
-                sum+=temp;
-                num/=10;
-            }
-            num = sum;
-        }
-        return num;
+        if(num==0) return 0;
+        if(num%9==0) return 9;
+        int ans = num%9;
+        return ans;
+       
         
     }
 };
