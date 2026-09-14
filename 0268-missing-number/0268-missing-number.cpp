54f040dc21885bc389c28181d07503c733f03a2e
MD Shayaan Arif <mohammadshayaan2030@gmail.com>
2026-09-14 11:21:25 +0530

    Time: 6 ms (16.14%), Space: 21.6 MB (93.82%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i;
+        for(i=0;i<nums.size();i++){
+            if(i == nums[i]){
+                continue;
+            }
+            else{
+                return i;
+            }
+        }
+        return i;
+        
+        
+    }
+};
