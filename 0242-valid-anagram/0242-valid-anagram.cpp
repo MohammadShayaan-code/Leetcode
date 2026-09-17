52e0722458db2fa9c05620eb3a9f2d4a01402fbb
MD Shayaan Arif <mohammadshayaan2030@gmail.com>
2026-09-17 22:38:01 +0530

    Time: 1 ms (53.61%), Space: 10 MB (11.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
 
+        if(s.size()!=t.size()) return false;
+        
+        unordered_map<char , int> hash;
         for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
+            hash[s[i]]++;
+        }
+       
+        for(int i=0;i<t.size();i++){
+            hash[t[i]]--;
+        }
+        
+        for(auto it:hash){
+            if(it.second > 0) return false;
         }
         return true;
+        
+
+        
         
     }
 };
